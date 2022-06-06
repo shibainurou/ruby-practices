1864632fa0b6ea269762ad8ca9c7eb8b3225938a
shibainurou <shibainurou@gmail.com>
2022-06-06 22:27:22 +0900

    配列の添え字を文字列からシンボルに変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,28 +4,23 @@ require 'date'
 
 # param
 begin
-  params = ARGV.getopts("y:m:")
+  params = Hash[ARGV.getopts('y:m:').map{ |k, v| [k.to_sym, v] }]
 rescue
   params = {}
 end
-params["y"] ||= Date.today.year
+params[:y] ||= Date.today.year
 
-if params["y"].nil?
-  params["y"] = Date.today.year
-end
-if params["m"].nil? || !(1 <= params["m"].to_i && params["m"].to_i <= 12)
-  params["m"] = Date.today.month
+if params[:m].nil? || !(1 <= params[:m].to_i && params[:m].to_i <= 12)
+  params[:m] = Date.today.month
 end
 
-params["y"] = params["y"].to_i
-params["m"] = params["m"].to_i
-
-
+params[:y] = params[:y].to_i
+params[:m] = params[:m].to_i
 CHAR_SIZE = 2
 MARGIN = 1
 SCR_WIDTH = (7 * CHAR_SIZE) + (MARGIN * 6)
 
-title = "#{params["m"]}月  #{params["y"]}"
+title = "#{params[:m]}月  #{params[:y]}"
 header = ["日", "月", "火", "水", "木", "金", "土"]
 
 # title
@@ -35,8 +30,8 @@ puts title.center(SCR_WIDTH)
 puts header.join(" " * MARGIN)
 
 # calender
-firstDate = Date.new(params["y"], params["m"], 1)
-lastDate = Date.new(params["y"], params["m"], -1)
+firstDate = Date.new(params[:y], params[:m], 1)
+lastDate = Date.new(params[:y], params[:m], -1)
 print " " * (firstDate.wday * (CHAR_SIZE + MARGIN))
 
 (firstDate..lastDate).each do |date|
