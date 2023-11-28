b8fe87ee8c36af07936a791e5e8c3c2711d79eaf
shibainurou <shibainurou@gmail.com>
2023-11-28 22:06:32 +0900

    オブジェクト指向版ボウリングスコア計算 作成完了

---
+++ 07.bowling_object/frame.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require_relative './Shot'
+
+class Frame
+  attr_reader :score
+  attr_accessor :shots
+
+  def initialize
+    @shots = []
+    @shot_count = 0
+    @score = 0
+    @bonus_count = 0
+  end
+
+  def take_shot(score)
+    @shots << Shot.new(score)
+    @score += @shots.last.score
+
+    @bonus_count = 2 if strike?
+    @bonus_count = 1 if spare?
+
+    @shot_count += 1
+  end
+
+  def strike?
+    @shots.first.score == 10
+  end
+
+  def spare?
+    @shots.size == 2 && @score == 10
+  end
+
+  def bonus_complate?
+    @bonus_count.zero?
+  end
+
+  def calculate_bonus(bonus_frames)
+    bonus_frames.each do |bonus_frame|
+      bonus_frame.add_bounus_score(@shots[@shot_count - 1].score)
+    end
+  end
+
+  def add_bounus_score(score)
+    @score += score
+    @bonus_count -= 1
+  end
+end
---
+++ 07.bowling_object/game.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require_relative './Frame'
+
+class Game
+  attr_accessor :frame
+
+  MAX_FRAMES_PER_GAME = 10
+  MAX_SHOTS_PER_FRAME = 3
+
+  def initialize
+    @frames = []
+    @bonus_frames = []
+    MAX_FRAMES_PER_GAME.times do
+      @frames << Frame.new
+    end
+  end
+
+  def run(pins)
+    @frames.each_with_index do |frame, frame_index|
+      MAX_SHOTS_PER_FRAME.times do
+        play_frame(pins.shift, frame)
+
+        @bonus_frames << frame if !last_frame?(frame_index) && (frame.strike? || frame.spare?)
+
+        break if !last_frame?(frame_index) && (frame.strike? || frame.shots.size >= 2)
+      end
+    end
+  end
+
+  def play_frame(score, frame)
+    frame.take_shot(score)
+
+    return if @bonus_frames.empty?
+
+    frame.calculate_bonus(@bonus_frames)
+    @bonus_frames.reject!(&:bonus_complate?)
+  end
+
+  # Gameクラスが全体の流れを制御しているので
+  # 最終フレーム or NOTはGameクラスで判断する
+  def last_frame?(frame_index)
+    frame_index == MAX_FRAMES_PER_GAME - 1
+  end
+
+  def total_score
+    @frames.inject(0) { |sum, frame| sum + frame.score }
+  end
+end
