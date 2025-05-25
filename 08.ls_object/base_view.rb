63ad7f4e3b3a6f614cdf34b453e5ed33a45541dd
shibainurou <shibainurou@gmail.com>
2025-05-25 22:37:52 +0900

    displayとmax_lenをStructから配列に変更 文字列の最大長の取得をイテレータを使って取得できるようにするため

--- 08.ls_object/base_view.rb
+++ 08.ls_object/base_view.rb
@@ -3,8 +3,8 @@
 class BaseView
   def initialize(file_names)
     @file_names = file_names
-    @file_infos = file_names.map do |filename|
-      File.lstat(filename)
+    @file_infos = file_names.map do |file_name|
+      File.lstat(file_name)
     end
   end
 
--- 08.ls_object/list_view.rb
+++ 08.ls_object/list_view.rb
@@ -4,8 +4,19 @@ require 'etc'
 require_relative 'base_view'
 
 class ListView < BaseView
-  Struct.new('Display', :filename, :type, :permission, :hardlink, :owner, :group, :filesize, :timestamp, :blocks)
-  Struct.new('MaxLenght', :filename, :type, :permission, :hardlink, :owner, :group, :filesize, :timestamp)
+  LEN_INDEX_MAX = 8
+
+  module DisplayIndex
+    FILE_NAME = 0
+    TYPE = 1
+    PERMISSION = 2
+    HARD_LINK = 3
+    OWNER = 4
+    GROUP = 5
+    FILE_SIZE = 6
+    TIMESTAMP = 7
+    BLOCKS = 8
+  end
 
   FILE_TYPE_STR = {
     'file' => '-',
@@ -28,50 +39,44 @@ class ListView < BaseView
   def render
     display_file_infos = display_file_info
     row_max_len = row_max_lenght(display_file_infos)
-
-    puts "total #{display_file_infos.map(&:blocks).sum}"
+    puts "total #{display_file_infos.map { |v| v[DisplayIndex::BLOCKS] }.sum}"
     display_file_infos.each do |file|
-      print file.type.rjust(row_max_len.type)
-      print "#{file.permission.rjust(row_max_len.permission)}  "
-      print "#{file.hardlink.rjust(row_max_len.hardlink)} "
-      print "#{file.owner.ljust(row_max_len.owner)}  "
-      print "#{file.group.ljust(row_max_len.group)}  "
-      print "#{file.filesize.to_s.rjust(row_max_len.filesize)} "
-      print "#{file.timestamp.rjust(row_max_len.timestamp)} "
-      print file.filename
-      print " -> #{File.readlink(file.filename)}" if file.type == 'l'
+      print file[DisplayIndex::TYPE].rjust(row_max_len[DisplayIndex::TYPE])
+      print "#{file[DisplayIndex::PERMISSION].rjust(row_max_len[DisplayIndex::TYPE])}  "
+      print "#{file[DisplayIndex::HARD_LINK].rjust(row_max_len[DisplayIndex::HARD_LINK])} "
+      print "#{file[DisplayIndex::OWNER].ljust(row_max_len[DisplayIndex::OWNER])}  "
+      print "#{file[DisplayIndex::GROUP].ljust(row_max_len[DisplayIndex::GROUP])}  "
+      print "#{file[DisplayIndex::FILE_SIZE].to_s.rjust(row_max_len[DisplayIndex::FILE_SIZE])} "
+      print "#{file[DisplayIndex::TIMESTAMP].rjust(row_max_len[DisplayIndex::TIMESTAMP])} "
+      print "#{file[DisplayIndex::FILE_NAME]} "
+      print " -> #{File.readlink(file[DisplayIndex::FILE_NAME])}" if file[DisplayIndex::TYPE] == 'l'
       print "\n"
     end
   end
 
   def display_file_info
-    @file_names.map do |f|
-      d = Struct::Display.new(f)
-      stat = File.lstat(f)
-      d.filename = f
-      d.type = FILE_TYPE_STR[stat.ftype]
-      d.permission = print_permission(stat.mode, PERMISSION_STR)
-      d.hardlink = stat.nlink.to_s
-      d.owner = Etc.getpwuid(stat.uid).name
-      d.group = Etc.getgrgid(stat.gid).name
-      d.filesize = stat.size
-      d.timestamp = stat.mtime.strftime('%_m %_d %H:%M')
-      d.blocks = stat.blocks
-      d
+    @file_names.map do |file_name|
+      display = []
+      stat = File.lstat(file_name)
+      display[DisplayIndex::FILE_NAME] = file_name
+      display[DisplayIndex::TYPE] = FILE_TYPE_STR[stat.ftype]
+      display[DisplayIndex::PERMISSION] = print_permission(stat.mode, PERMISSION_STR)
+      display[DisplayIndex::HARD_LINK] = stat.nlink.to_s
+      display[DisplayIndex::OWNER] = Etc.getpwuid(stat.uid).name
+      display[DisplayIndex::GROUP] = Etc.getgrgid(stat.gid).name
+      display[DisplayIndex::FILE_SIZE] = stat.size
+      display[DisplayIndex::TIMESTAMP] = stat.mtime.strftime('%_m %_d %H:%M')
+      display[DisplayIndex::BLOCKS] = stat.blocks
+      display
     end
   end
 
   def row_max_lenght(display_file_infos)
-    max_len = Struct::MaxLenght.new(0, 0, 0, 0, 0, 0, 0, 0)
+    max_len = []
     display_file_infos.each do |v|
-      max_len.filename = [max_len.filename, v.filename.length].max
-      max_len.type = [max_len.type, v.type.length].max
-      max_len.permission = [max_len.permission, v.permission.length].max
-      max_len.hardlink = [max_len.hardlink, v.hardlink.length].max
-      max_len.owner = [max_len.owner, v.owner.length].max
-      max_len.group = [max_len.group, v.group.length].max
-      max_len.filesize = [max_len.filesize, v.filesize.to_s.length].max
-      max_len.timestamp = [max_len.timestamp, v.timestamp.length].max
+      LEN_INDEX_MAX.times do |index|
+        max_len[index] = [max_len[index].to_i, v[index].to_s.length].max
+      end
     end
     max_len
   end
